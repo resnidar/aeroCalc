14aa6435605c3cebc96d362bf16dcc39cb322775
Gros <swagthepotatoes74@gmail.com>
2020-12-18 17:41:35 +0100

    Fonction temps restant ajouté.

--- src/main/java/com/resnidar/aeroCalc/Endurance.java
+++ src/main/java/com/resnidar/aeroCalc/Endurance.java
@@ -3,8 +3,10 @@ package com.resnidar.aeroCalc;
 public class Endurance implements EnduranceInterface{
     @Override
     public String tempsRestant(int carburant, int consommation) {
-        //TODO temps restant en heure
-        return "fonctionnement ok";
+        float time = 60*carburant / consommation;
+        int minute = (int)(time % 60);
+        int heure = (int)(time - minute)/60;
+        return "Temps restant :" + heure +"H et " + minute + "min";
     }
 
     @Override
