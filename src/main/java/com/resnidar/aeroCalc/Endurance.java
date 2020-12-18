a9031387448b7f1d2a4199336388e7898dd4bf0f
resnidar <remy.snidaro@gmail.com>
2020-12-18 03:59:43 +0100

    debut calcul endurance.jsp

---
+++ src/main/java/com/resnidar/aeroCalc/Endurance.java
@@ -0,0 +1,15 @@
+package com.resnidar.aeroCalc;
+
+public class Endurance implements EnduranceInterface{
+    @Override
+    public String tempsRestant(int carburant, int consommation) {
+        //TODO temps restant en heure
+        return null;
+    }
+
+    @Override
+    public int distanceRestant(int carburant, int consommation, int vitesse) {
+        //TODO distance en NM
+        return 0;
+    }
+}
